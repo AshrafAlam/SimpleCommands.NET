e8656042c478b5b8b382107ff0eda9199f2e8a80
Ashraf Alam <ashraf.alam@schroders.com>
2021-04-30 10:09:52 +0100

    Add initial code.

---
+++ Src/Json2MD.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Json2MD.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
