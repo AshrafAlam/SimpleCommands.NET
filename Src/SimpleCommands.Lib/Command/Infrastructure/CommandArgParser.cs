0205d57dbdbb19d37d79227d9382761cafb29282
Ashraf Alam <ashraf.alam@schroders.com>
2021-06-29 17:22:57 +0100

    -- updated excptions

--- Src/SimpleCommands.Lib/Command/Infrastructure/CommandArgParser.cs
+++ Src/SimpleCommands.Lib/Command/Infrastructure/CommandArgParser.cs
@@ -1,6 +1,4 @@
-﻿using SimpleCommands.Core.Exceptions;
-using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace SimpleCommands.Core.Command.Infrastructure
 {
@@ -19,38 +17,6 @@ namespace SimpleCommands.Core.Command.Infrastructure
                 CommandName = commandName,
                 CommandArgs = commandArgs
             };
-        }
-
-        public static int[] ParseToIntArray(this string[] args, int? start = null, int? end = null)
-        {
-            if (args == null)
-                throw new ArgumentNullException(nameof(args));
-
-            if (start == null)
-                start = 0;
-
-            if (start < 0)
-                throw new ArgumentOutOfRangeException(nameof(start));
-
-            if (end == null)
-                end = args.Length - 1;
-
-            if (end >= args.Length)
-                throw new ArgumentOutOfRangeException(nameof(end));
-
-            int[] intArray = new int[(int)(end - start) + 1];
-
-            int intArrayCounter = 0;
-
-            for (var i = (int)start; i <= end; i++)
-            {
-                if (!int.TryParse(args[i], out var intValue))
-                    ExceptionThrower.Throws<InvalidCommandArgumentFormatException>();
-
-                intArray[intArrayCounter++] = intValue;
-            }
-
-            return intArray;
         }
     }
 }
