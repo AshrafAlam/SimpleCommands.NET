21295ab305539751d05d5f11e52f91b0fbc83304
Alam, Ashraf <joy_csharp@yahoo.com>
2021-07-01 11:02:30 +0100

    -- added new tests with empty argument

--- Src/SimpleCommands.Tests/CommandStreamProcessorTests_HelloCommand.cs
+++ Src/SimpleCommands.Tests/CommandStreamProcessorTests_HelloCommand.cs
@@ -47,5 +47,22 @@ namespace SimpleCommands.Tests
             Assert.Equal(expectedOutput, actualOutput);
         }
 
+        [Fact]
+        public void ProcessCommands_HelloCommandsPassedWithNoParam_ShouldDisplayError()
+        {
+            //Arrange 
+            var commandStreamProcessor =
+                Factory_CommandStreamProcessor("ProcessCommands_Hello_NoParam_Input.txt");
+            var expectedOutput = TestDataReader.LoadAsString("ProcessCommands_Hello_NoParam_Output.txt");
+
+            //Act
+            commandStreamProcessor.ProcessCommands();
+
+            //Get output
+            var actualOutput = commandStreamProcessor.GetOutput().ToString();
+
+            //Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
     }
 }
